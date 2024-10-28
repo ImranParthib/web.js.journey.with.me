141ccd51e9d806f2ef189373a300764bb8bb0086
ImranParthib <imranparthib13@gmail.com>
2024-10-29 00:41:22 +0600

    🚀 Refactor Apply Transform Over Each Element in Array.js to use map function

--- LeetCode Problem Solving/30 Days of JavaScript/Apply Transform Over Each Element in Array.js
+++ LeetCode Problem Solving/30 Days of JavaScript/Apply Transform Over Each Element in Array.js
@@ -1,12 +1,30 @@
-/**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
- */
+// /**
+//  * @param {number[]} arr
+//  * @param {Function} fn
+//  * @return {number[]}
+//  */
+// var map = function (arr, fn) {
+//     let newArray = [];
+//     arr.forEach((element, index) => {
+//         newArray.push(fn(element, index));
+//     });
+//     return newArray;
+// };
+// ?Given
+arr = [1, 2, 3];
+function plusone(n) {
+  return n + 1;
+}
+
+// console.log(map([1, 2, 3, 4, 5], plusone)); // [2, 3, 4, 5, 6]
+
 var map = function (arr, fn) {
-    let newArray = [];
-    arr.forEach((element, index) => {
-        newArray.push(fn(element, index));
-    });
-    return newArray;
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(fn(arr[i], i));
+  }
+  return newArray;
 };
+
+let result = map(arr,plusone);
+console.log(result);
