e90227b2a27f1e2742e1c81f2d309ed479b2c980
ImranParthib <imranparthib13@gmail.com>
2025-01-09 23:58:53 +0600

    ✨ Add filter method implementation to filter even numbers from an array

---
+++ LearningFundamentals/ArrayMethods/filter.js
@@ -0,0 +1,12 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function testFunction(num) {
+  return num % 2 === 0;
+}
+
+const filteredValue = arr.filter(testFunction);
+
+console.log(filteredValue);  
+ 
+ 
+ 
