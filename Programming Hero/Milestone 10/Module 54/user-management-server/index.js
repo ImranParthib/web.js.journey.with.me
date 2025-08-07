5e31c58e7529d26bcb47b2d6ecd5d4b016e7151b
Imran Parthib <imranparthib13@gmail.com>
2025-08-08 01:25:05 +0600

    ✨ Add user data and update root route message in user management server

--- Programming Hero/Milestone 10/Module 54/user-management-server/index.js
+++ Programming Hero/Milestone 10/Module 54/user-management-server/index.js
@@ -3,8 +3,23 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const user = [
+  {
+    id:1,
+    name:"Imran Parthin"
+  },
+  {
+    id:2,
+    name:"Mehedi Hasan Khan"
+  }
+]
+
 app.get("/", (req, res) => {
-  res.send("User Management Server is running successfully!");
+  res.send("User Management Server, Go the /user route");
+});
+
+app.get("/user", (req, res) => {
+  res.send(user);
 });
 
 app.listen(PORT, () => {
